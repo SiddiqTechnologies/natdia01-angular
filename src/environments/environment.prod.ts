c4abbb48c88df96f9fcc38551611773fcef5e891
Siddiq Technologies <siddiqtechnologies@yahoo.com>
2020-02-18 12:37:52 -0800

    Test mac In Cloud coding

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,13 @@
 export const environment = {
   production: true,
   firebaseConfig: {
-    apiKey: "AIzaSyC7Vh9tDD6bcRFLCC7u9wS43L8x36Y5jwQ",
-    authDomain: "siddiqangulartesting.firebaseapp.com",
-    databaseURL: "https://siddiqangulartesting.firebaseio.com",
-    projectId: "siddiqangulartesting",
-    storageBucket: "siddiqangulartesting.appspot.com",
-    messagingSenderId: "674612391821",
-    appId: "1:674612391821:web:2ee1aaac2117547e3157c4",
-    measurementId: "G-ZT7V7RE6QG"
+      apiKey: "AIzaSyCSlHO69zXGqgKt25b7mpHostChDFdKJ1I",
+      authDomain: "nationaldianetics.firebaseapp.com",
+      databaseURL: "https://nationaldianetics.firebaseio.com",
+      projectId: "nationaldianetics",
+      storageBucket: "nationaldianetics.appspot.com",
+      messagingSenderId: "76306478675",
+      appId: "1:76306478675:web:edb09120d22bfe15",
+      measurementId: "G-TP55K844ZG"
   }
 };
