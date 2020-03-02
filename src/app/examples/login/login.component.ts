b00c0a30b5495c8e977e83f345a61a460da6c617
siddiqtech <siddiqtechnologies@yahoo.com>
2020-03-02 11:25:46 -0800

    CSS changes to login page

--- src/app/examples/login/login.component.ts
+++ src/app/examples/login/login.component.ts
@@ -3,6 +3,10 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 import { AngularFireAuth } from '@angular/fire/auth';
 import { Router } from '@angular/router';
+
+import { ModalService } from '../../services/modal.service';
+import { take } from 'rxjs/operators';
+
 import * as firebase from 'firebase';
 
 @Component({
@@ -17,9 +21,10 @@ export class LoginComponent implements OnInit {
   focus1;
   serverMessage: string;
   analytics = firebase.analytics();
+  messageResult: boolean;
 
   constructor(private afAuth: AngularFireAuth, private fb: FormBuilder,
-    private router: Router) { }
+    private router: Router, private kModal: ModalService) { }
 
   ngOnInit() {
     // console.log(this.form);
@@ -58,9 +63,33 @@ export class LoginComponent implements OnInit {
         this.router.navigate(['/hours']).catch(err => console.log(err));
       });
     } catch (err) {
-      this.serverMessage = err;
+      this.serverMessage = err.code;
+      if(this.serverMessage === "auth/user-not-found") {
+        this.serverMessage = "There is no user account registered to that email. Please check and try again."
+      } else if (this.serverMessage === "auth/wrong-password"){
+        this.serverMessage = "Wrong username/password. Please check and try again."
+      } else {
+        this.serverMessage
+      }
       console.log(this.serverMessage);
+      this.kModal.message(
+        this.serverMessage
+      ).pipe(
+        take(1) // take() manages unsubscription for us
+      ).subscribe(result => {
+        this.messageResult = result;
+          console.log(this.messageResult);
+          this.router.navigate(['/login']).catch(err => console.log(err));
+        });
     }
+  }
+
+  handleRegisterClick = () => {
+    alert('working');
+  }
+
+  handleForgotClick = () => {
+    alert('working');
   }
 
 }
